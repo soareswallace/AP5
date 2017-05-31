5e497f0d632cb341d96f2e62c5caccbf6a0c28f5
Wallace <soareswallace@icloud.com>
2017-05-30 21:41:24 -0300

    Aprendizado

--- aprendizado_motherfucker.asm
+++ aprendizado_motherfucker.asm
@@ -1,10 +1,19 @@
-extern imprimir
+extern imprimir, intparserfloat, pegan ;Funcoes Externas
 SECTION .data
-	var dq 3.5
+	var dd 0
+	n 	dd 4
 SECTION .text
 global main
 main:
-	push dword [var+4]
-	push dword [var]
-	call imprimir
-	add esp, 8
+	call pegan ;Chama a funcao em C, que retorna um numero inteiro
+	push eax ;retorno de funcao Inteiro eh guardado no EAX, por isso empilhamos a conversao
+	call intparserfloat ;chama a funcao que ira converter de inteiro para float, o retorno vai para st0
+	add esp, 4 ; como passamos 1 parametro, ajustamos pilha por 4, se fossem 2 - 8 e...
+	fst dword[var] ;colocamos o resultado em float(st0) na variavel var
+	push dword[var] ;empilha var para que possa ser impressa
+	call imprimir ; funcao imprimir
+	add esp, 4 ;reajustando a pilha voltando ao ESP antigo
+
+	mov eax, 1
+	mov ebx, 0
+	int 80h
--- imprimir.c
+++ imprimir.c
@@ -1,3 +1,21 @@
-void imprimir(double valor){
-	printf("%lf\n", valor);
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprimir(float valor){
+	printf("O valor eh: %f\n, valor");
+}
+//essa funcao pode ser util na prova porque ela testa
+//cuidado para nao por um inteiro na fpu. nao pode
+
+float intparserfloat(int valor){
+	float convertido;
+	convertido = (float)valor;
+	return convertido;
+}
+
+int pegan(){
+	printf("Insira o numero que sera convertido para float\n");
+	int num = 0;
+	scanf(" %d", &num);
+	return num;
 }
